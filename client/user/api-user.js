827638f8b7d5a428753d5e48526e009aabd41ceb
mohammadameer <mohammadameerabdallah@gmail.com>
2019-12-20 20:50:27 +0200

    add the user api

--- client/user/api-user.js
+++ client/user/api-user.js
@@ -12,3 +12,53 @@ const create = user => {
     })
     .catch(err => console.error(err));
 };
+
+const list = () => {
+  return fetch("/api/users/", {
+    method: "GET"
+  })
+    .then(response => response.json())
+    .catch(err => console.error(err));
+};
+
+const read = (params, credentials) => {
+  return fetch("/api/users/" + params.userId, {
+    method: "GET",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      Authorization: "Bearer " + credentials.t
+    }
+  })
+    .then(response => response.json())
+    .catch(err => console.error(err));
+};
+
+const update = (params, credentials, user) => {
+  return fetch("/api/users/" + params.userId, {
+    method: "PUT",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      Authorization: "Bearer " + credentials.t
+    },
+    body: JSON.stringify(user)
+  })
+    .then(response => response.json())
+    .catch(err => console.error(err));
+};
+
+const remove = (params, credentials) => {
+  return fetch("/api/users/" + params.userId, {
+    method: "DELETE",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      Authorization: "Bearer " + credentials.t
+    }
+  })
+    .then(response => response.json())
+    .catch(err => console.error(err));
+};
+
+export { create, list, update, remove };
